0b56e8de6702cbb856cde7326b03225b70b205bb
larsjensen <lgjensen5@gmail.com>
2023-05-16 18:27:49 +0200

    Better menu probably, is properly responsive.

---
+++ menu2.php
@@ -0,0 +1,12 @@
+<?php
+
+//This brings in a twig instance for use by this script
+require_once __DIR__.'/bootstrap.php';
+require_once __DIR__.'/database.php';
+
+//Load from the DB
+$db = new Db();
+
+$menu = $db -> select("SELECT * FROM MEALS");
+
+echo $twig->render('menu2.html', ['menu' => $menu]);
